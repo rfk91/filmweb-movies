aaaeeb3b26544a72471331f8623644d1b9b673a4
rfk91 <rfk9191@gmail.com>
2020-07-16 18:55:45 +0100

    vscode settings & format

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import * as fs from 'fs';
 import * as pptr from 'puppeteer';
-import { asyncForEach } from './async-foreach'
+import { asyncForEach } from './async-foreach';
 
 const dir: string = './gen/';
 
-const URL: string = 'https://www.filmweb.pl/films/search?orderBy=popularity&descending=true';
+const URL: string =
+  'https://www.filmweb.pl/films/search?orderBy=popularity&descending=true';
 
 const viewport: pptr.Viewport = {
   width: 800,
@@ -21,13 +22,13 @@ interface Movie {
   originalTitle: string;
   description: string;
   rate: string;
-  year: string
+  year: string;
 }
 
 (async () => {
   try {
     const browser = await pptr.launch({
-      headless: true
+      headless: true,
     });
 
     const page = await browser.newPage();
@@ -35,7 +36,7 @@ interface Movie {
     await page.setViewport(viewport);
 
     await page.goto(`${URL}&page=1`, {
-      waitUntil: 'networkidle0'
+      waitUntil: 'networkidle0',
     });
 
     // Close GDPR popup
@@ -50,21 +51,25 @@ interface Movie {
 
       if (i > 1) {
         await page.goto(`${URL}&page=${i}`, {
-          waitUntil: 'networkidle0'
+          waitUntil: 'networkidle0',
         });
       }
       await page.screenshot({
-        path: `${dir}page-${i}.png`
+        path: `${dir}page-${i}.png`,
       });
       const movies = await grabMovies(page);
       allMovies.push(movies);
     }
 
     const mergedMovies = [].concat(...allMovies);
-    fs.writeFile(`${dir}result.json`, JSON.stringify(mergedMovies), (err: any) => {
-      if (err) throw err;
-      console.log('result.json file saved ok!');
-    });
+    fs.writeFile(
+      `${dir}result.json`,
+      JSON.stringify(mergedMovies),
+      (err: any) => {
+        if (err) throw err;
+        console.log('result.json file saved ok!');
+      }
+    );
 
     const meta = {
       length: mergedMovies.length,
@@ -75,8 +80,7 @@ interface Movie {
     });
 
     await browser.close();
-  }
-  catch (error) {
+  } catch (error) {
     console.log(error);
   }
 })();
@@ -89,11 +93,11 @@ async function grabMovies(page: pptr.Page) {
   await asyncForEach(moviesEl, async (movieEl) => {
     const posterEl = await movieEl.$('.filmPreview__poster .poster__image');
     const posterElProp = await posterEl.getProperty('src');
-    const poster: string = await posterElProp.jsonValue() as string;
+    const poster: string = (await posterElProp.jsonValue()) as string;
 
     const titleEl = await movieEl.$('.filmPreview__title');
     const titleElProp = await titleEl.getProperty('innerText');
-    const title: string = await titleElProp.jsonValue() as string;
+    const title: string = (await titleElProp.jsonValue()) as string;
 
     // Log movie titles as we go...
     console.log(title);
@@ -103,21 +107,23 @@ async function grabMovies(page: pptr.Page) {
     if (!originalTitleEl) {
       originalTitle = title;
     } else {
-      const originalTitleElProp = await originalTitleEl.getProperty('innerText');
-      originalTitle = await originalTitleElProp.jsonValue() as string;
+      const originalTitleElProp = await originalTitleEl.getProperty(
+        'innerText'
+      );
+      originalTitle = (await originalTitleElProp.jsonValue()) as string;
     }
 
     const descriptionEl = await movieEl.$('.filmPreview__description > p');
     const descriptionElProp = await descriptionEl.getProperty('innerText');
-    const description: string = await descriptionElProp.jsonValue() as string;
+    const description: string = (await descriptionElProp.jsonValue()) as string;
 
     const rateEl = await movieEl.$('.rateBox__rate');
     const rateElProp = await rateEl.getProperty('innerText');
-    const rate: string = await rateElProp.jsonValue() as string;
+    const rate: string = (await rateElProp.jsonValue()) as string;
 
     const yearEl = await movieEl.$('.filmPreview__year');
     const yearElProp = await yearEl.getProperty('innerText');
-    const year: string = await yearElProp.jsonValue() as string;
+    const year: string = (await yearElProp.jsonValue()) as string;
 
     movies.push({
       poster,
@@ -125,7 +131,7 @@ async function grabMovies(page: pptr.Page) {
       originalTitle,
       description,
       rate,
-      year
+      year,
     });
   });
 
